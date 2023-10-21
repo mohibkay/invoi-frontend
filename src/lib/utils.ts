a3f89da577bdede3b5ff6780fde682f70292e766
mohibkay <mohibk.in@gmail.com>
2023-10-21 14:04:18 +0530

    add file name generator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateInvoiceFilename(uniqueIdentifier: string) {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const filename = `${year}${month}${day}-${uniqueIdentifier}-Invoice`;
+  return filename;
 }
