8925fa02876a332e736cf1c5a7da8392e1b1e7f2
mohibkay <mohibk.in@gmail.com>
2023-10-29 19:45:24 +0530

    add WellnessExpense type

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,6 +6,20 @@ type Invoice = {
   amount: string;
 };
 
+type WellnessExpense = {
+  Description: string;
+  "Spent At": string;
+  Category: string;
+  Currency: string;
+  City: string;
+  "Txn Amount": number;
+  Wallet: string;
+  "Date(YYYY-MM-DD)": string;
+  "Invoice Number": string;
+  "Invoice Date": string;
+  Client: string;
+};
+
 interface InvoiceListProp {
   invoiceDataArray: Invoice[];
 }
