7cdb028d5de47da2eec84121a88486505e3393dc
mohibkay <mohibk.in@gmail.com>
2023-12-06 12:51:11 +0530

    feat: add clear user dispatch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,23 @@ import store from "storejs";
 import { useQueryClient } from "@tanstack/react-query";
 import { ROUTES } from "@/lib/routes";
 import { useNavigate, Link } from "react-router-dom";
-
-import { useAppSelector } from "@/redux/hooks";
+import { clearUser } from "@/redux/features/userSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { Button } from "./ui/button";
 import { Icons } from "./utils/Icons";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
+
   const { credits = 0 } = user.user || {};
 
   const handleLogout = () => {
     store.clear();
     queryClient.clear();
+    dispatch(clearUser());
     navigate(ROUTES.LOGIN);
   };
 
@@ -25,10 +28,14 @@ const Navbar = () => {
         Invoi
       </Link>
 
-      <p className='text-lg'>Credits: {credits}</p>
-      <Button variant='link' onClick={handleLogout}>
-        <Icons.logout />
-      </Button>
+      {user.user && (
+        <>
+          <p className='text-lg'>Credits: {credits}</p>
+          <Button variant='link' onClick={handleLogout}>
+            <Icons.logout />
+          </Button>
+        </>
+      )}
     </nav>
   );
 };
